a4119c2068bacc3576557531acfc2535fa1487b9
fantastic-413 <57306219+fantastic-413@users.noreply.github.com>
2020-09-19 20:13:54 +0800

    初始

---
+++ styleOfMe/cmake-build-debug/CMakeFiles/styleOfMe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/styleOfMe.dir/Task.cpp.obj"
+  "CMakeFiles/styleOfMe.dir/main.cpp.obj"
+  "libstyleOfMe.dll.a"
+  "styleOfMe.exe"
+  "styleOfMe.exe.manifest"
+  "styleOfMe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/styleOfMe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ styleOfMe/cmake-build-debug/Task.cpp
@@ -0,0 +1,9 @@
+//
+// Created by L&S&P& on 2020/9/19.
+//
+
+#include "Task.h"
+
+Task Task::setTask() {
+
+}
